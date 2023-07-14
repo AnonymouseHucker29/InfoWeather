7a6cfaf5858ec3a779963a4456b16daf7c8eb7f7
AnonymouseHucker29 <brent.baylon29@gmail.com>
2023-07-14 20:41:50 +0800

    v1.5.0 added additional weather info

---
+++ src/handlers/misc/timezoneHandler.js
@@ -0,0 +1,4 @@
+export function timezoneHandler(offsetMinutes) {
+    const timezones = moment.tz.names();
+    return timezones.filter((zoneName) => moment.tz(zoneName).utcOffset() === offsetMinutes);
+}
--- src/handlers/weatherHandler.js
+++ src/handlers/weatherHandler.js
@@ -1,84 +1,96 @@
 import { callSendHandler } from './callSendHandler.js'
 import { delayHandler } from './delayHandler.js'
 import { markMessageAsSeen, showTypingIndicator } from './typingAndSeenIndicator.js'
+import { timezoneHandler } from './misc/timezoneHandler.js'
+import moment from 'moment-timezone'
 import dotenv from 'dotenv'
 
 dotenv.config();
 
 export async function weatherHandler(sender_psid, received_message, message_id) {
-    let location = received_message;
-
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(location)}&appid=${process.env.OPENWEATHERMAP_API_KEY}`;
-
     try {
+        let location = received_message;
+
+        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(location)}&appid=${process.env.OPENWEATHERMAP_API_KEY}`;
         const response = await fetch(apiUrl);
         const weatherData = await response.json();
 
-        if (response.status === 200) {
-            if (location.toLowerCase() !== weatherData.name.toLowerCase()) {
-
-                await markMessageAsSeen(sender_psid, message_id);
-                await showTypingIndicator(sender_psid, received_message.mid);
-
-                location = weatherData.name;
-
-                const locationMessage = {
-                    "text": `I couldn't find the weather for ${received_message}. Here's the weather for ${location} instead.`
-                };
-                callSendHandler(sender_psid, locationMessage);
-            }
-
-            // Kelvin to Celsius conversion
-            const celsius = Math.round(weatherData.main.temp - 273.15);
-
-            // Kelvin to Fahrenheit conversion
-            const fahrenheit = Math.round((weatherData.main.temp - 273.15) * 9 / 5 + 32);
-
-            await delayHandler(1000);
-
+        if (response.status !== 200) {
             await markMessageAsSeen(sender_psid, message_id);
             await showTypingIndicator(sender_psid, received_message.mid);
-
-            const desc = weatherData.weather[0].description;
-
-            const weatherMessage1 = {
-                "text": `The weather in ${weatherData.name}, ${weatherData.sys.country} is ${celsius}°C or ${fahrenheit}°F having ${desc}.`
-            };
-            callSendHandler(sender_psid, weatherMessage1);
-
-            await delayHandler(1000);
-
-            await markMessageAsSeen(sender_psid, message_id);
-            await showTypingIndicator(sender_psid, received_message.mid);
-
-            const weatherMessage2 = {
-                "text": 'Do you want me to provide weather info for another location?',
-                "quick_replies": [
-                    {
-                        "content_type": "text",
-                        "title": "Yes",
-                        "payload": "OTHER_LOCATIONS_YES"
-                    },
-                    {
-                        "content_type": "text",
-                        "title": "No",
-                        "payload": "OTHER_LOCATIONS_NO"
-                    }
-                ]
-            };
-            callSendHandler(sender_psid, weatherMessage2);
-        } else {
-
-            await showTypingIndicator(sender_psid, received_message.mid);
-
-            console.error("Unable to send message:" + response.status);
+            console.error('Unable to send message:', response.status);
 
             const errorMessage = {
                 "text": `There's no available weather data for "${location}".`
             };
-
             callSendHandler(sender_psid, errorMessage);
+            return;
         }
+        if (location.toLowerCase() !== weatherData.name.toLowerCase()) {
+            await markMessageAsSeen(sender_psid, message_id);
+            await showTypingIndicator(sender_psid, received_message.mid);
+
+            location = weatherData.name;
+
+            const locationMessage = {
+                "text": `I couldn't find the weather for ${received_message}. Here's the weather for ${location} instead.`
+            };
+            callSendHandler(sender_psid, locationMessage);
+        }
+
+        await markMessageAsSeen(sender_psid, message_id);
+        await showTypingIndicator(sender_psid, received_message.mid);
+        await delayHandler(1000);
+
+        const celsius = Math.round(weatherData.main.temp - 273.15);
+        const fahrenheit = Math.round((weatherData.main.temp - 273.15) * 9 / 5 + 32);
+        const humidity = weatherData.main.humidity;
+        const clouds = weatherData.clouds.all;
+        const desc = weatherData.weather[0].description;
+        const lat = weatherData.coord.lat;
+        const lon = weatherData.coord.lon;
+
+        // Convert the time zone offset to a valid time zone identifier
+        const timezoneOffsetMinutes = weatherData.timezone / 60;
+        const timezoneIdentifier = moment.tz(timezoneHandler(timezoneOffsetMinutes)[0]).format('Z');
+
+        // Get the times using the adjusted time zone identifier
+        const currentTime = moment.unix(weatherData.dt)
+            .utcOffset(timezoneIdentifier)
+            .format('LT');
+        const sunriseTime = moment.unix(weatherData.sys.sunrise)
+            .utcOffset(timezoneIdentifier)
+            .format('LT');
+        const sunsetTime = moment.unix(weatherData.sys.sunset)
+            .utcOffset(timezoneIdentifier)
+            .format('LT');
+
+        const weatherMessage = {
+            "text": `Weather update as of ${currentTime} (${weatherData.name} time):\n\nThe weather in ${weatherData.name}, ${weatherData.sys.country} (${lat}, ${lon}) is ${celsius}°C or ${fahrenheit}°F having ${desc}.
+                    \n\nAdditional informations:\n\nTimezone: ${timezoneHandler(timezoneOffsetMinutes)[0]}\nClouds: ${clouds}%\nHumidity: ${humidity}%\nSunrise: ${sunriseTime}\nSunset: ${sunsetTime}\n`
+        };
+        callSendHandler(sender_psid, weatherMessage);
+
+        await delayHandler(1000);
+        await markMessageAsSeen(sender_psid, message_id);
+        await showTypingIndicator(sender_psid, received_message.mid);
+
+        const confirmMessage = {
+            "text": 'Do you want me to provide weather info for another location?',
+            "quick_replies": [
+                {
+                    "content_type": "text",
+                    "title": "Yes",
+                    "payload": "OTHER_LOCATIONS_YES"
+                },
+                {
+                    "content_type": "text",
+                    "title": "No",
+                    "payload": "OTHER_LOCATIONS_NO"
+                }
+            ]
+        };
+        callSendHandler(sender_psid, confirmMessage);
     } catch (error) {
         console.error("Unable to send message:", error);
     }
